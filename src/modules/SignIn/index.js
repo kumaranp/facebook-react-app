ad7a7d5eaa7df08895744569d2d7b7820e7d0bef
Kumaran Pitchai <kumaran.pitchai@teledirectasia.com>
2020-10-10 12:57:43 +0800

    sample facebook react app

--- src/modules/SignIn/index.js
+++ src/modules/SignIn/index.js
@@ -1,11 +1,31 @@
 import React, { Fragment } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
-function SignIn() {
+
+function SignIn(props) {
+	
+  const Authenticate = (e) => {
+        e.preventDefault();
+        return props.history.push("/dashboard")
+  }
+	
   return (
     <Fragment>
-      <h4>Login Form Goes here</h4>
-      <Link to="dashboard">Log in</Link>
+			<div >
+				<form onSubmit={Authenticate}>
+                <h3>Sign In</h3>
+                <div className="form-group">
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+				<div className="form-group">
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+				</div>
+            </form>
+			</div>
     </Fragment>
   );
 };
